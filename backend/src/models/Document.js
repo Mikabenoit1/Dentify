d417a7f65c83b5a32681826a6ea5529f13139f02
alexandrecirurso <alexandre.cirurso.1@ens.etsmtl.ca>
2025-04-17 09:47:43 -0400

    new backend

--- backend/src/models/Document.js
+++ backend/src/models/Document.js
@@ -1,51 +1,50 @@
 module.exports = (sequelize, DataTypes) => {
-    const Document = sequelize.define('Document', {
-      id_document: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      id_utilisateur: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      type_document: {
-        type: DataTypes.STRING(30),
-        allowNull: false
-      },
-      nom_fichier: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-      },
-      chemin_fichier: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-      },
-      date_telechargement: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
-      },
-      est_verifie: {
-        type: DataTypes.CHAR(1),
-        defaultValue: 'N'
-      },
-      date_verification: {
-        type: DataTypes.DATEONLY,
-        allowNull: true
-      }
-    }, {
-      tableName: 'Document',
-      timestamps: false
+  const Document = sequelize.define('Document', {
+    id_document: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    id_utilisateur: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    type_document: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    nom_fichier: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    chemin_fichier: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    date_telechargement: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    est_verifie: {
+      type: DataTypes.CHAR(1),
+      defaultValue: 'N'
+    },
+    date_verification: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+    }
+  }, {
+    tableName: 'Document',
+    timestamps: false
+  });
+
+  Document.associate = (models) => {
+    Document.belongsTo(models.User, {
+      foreignKey: 'id_utilisateur',
+      as: 'utilisateur',
+      onDelete: 'CASCADE'
     });
-  
-    Document.associate = (models) => {
-      Document.belongsTo(models.User, {
-        foreignKey: 'id_utilisateur',
-        as: 'utilisateur',
-        onDelete: 'CASCADE'
-      });
-    };
-  
-    return Document;
   };
-  
+
+  return Document;
+};
